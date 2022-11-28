28ae038eceef20e8386487b858bec1d75d2741f6
Shreya <shreya2701@gmail.com>
2022-11-28 11:06:22 +0530

    Changes In Admin

--- Design/Backend/CI_Platform/CI_Platform/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CI_Platform.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
